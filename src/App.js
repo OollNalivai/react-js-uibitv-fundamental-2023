46bc3543c06c145ebdc35c57655f7cc7e132a5a7
VladislavOoll <apahiphip@mail.ru>
2023-03-27 21:52:14 +0300

    try: use state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+    const state = useState(0)
+    console.log(state)
+    // let likes = 0;
     return (
         <div className="App">
-
+            {/*<h1>{likes}</h1>*/}
+            {/*<button onClick={() => likes += 1}>Increment</button>*/}
+            {/*<button onClick={() => likes -= 1}>Decrement</button>*/}
         </div>
     );
 }
