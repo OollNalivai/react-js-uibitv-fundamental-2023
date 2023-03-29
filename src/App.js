9ad82f039053c2537a72b6e3152fe0b29fb4e0af
VladislavOoll <apahiphip@mail.ru>
2023-03-29 22:47:48 +0300

    fetch: try two-way binding react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 
 function App() {
     let [likes, setLikes] = useState(5)
+    const [value, setValue] = useState('Text in input')
 
     function increment () {
         setLikes(likes + 1);
@@ -13,6 +14,8 @@ function App() {
     return (
         <div className="App">
             <h1>{likes}</h1>
+            <h2>{value}</h2>
+            <input type="text" onChange={event => setValue(event.target.value)} value={value}/>
             <button onClick={decrement}>Decrement</button>
             <button onClick={increment}>Increment</button>
         </div>
