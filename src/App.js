be3b26eb19d51ac5f612054316349b8eb5c7b25e
VladislavOoll <apahiphip@mail.ru>
2023-04-01 14:44:09 +0300

    create: counter component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React, {useState} from 'react';
+import Counter from './components/Counter';
 
 function App() {
-    let [likes, setLikes] = useState(5);
     const [value, setValue] = useState('Text in input');
-
-    function increment() {
-        setLikes(likes + 1);
-    }
-
-    function decrement() {
-        setLikes(likes - 1);
-    }
 
     return (
         <div className="App">
-            <h1>{likes}</h1>
             <h2>{value}</h2>
             <input type="text" onChange={event => setValue(event.target.value)} value={value}/>
-            <button onClick={decrement}>Decrement</button>
-            <button onClick={increment}>Increment</button>
+            <Counter></Counter>
         </div>
     );
 }
