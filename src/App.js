6c4de601c2f8a51f39d39b05bbb755653536ab55
VladislavOoll <apahiphip@mail.ru>
2023-04-03 23:05:23 +0300

    fetch: post styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, {useState} from 'react';
 import Counter from './components/Counter';
 import ClassCounter from './components/ClassCounter';
+import './styles/App.css';
 
 function App() {
     const [value, setValue] = useState('Text in input');
 
     return (
         <div className="App">
-            <h2>{value}</h2>
-            <input type="text" onChange={event => setValue(event.target.value)} value={value}/>
-            <Counter/>
-            <ClassCounter/>
+            <div className="post">
+                <div className="post__content">
+                    <strong>1. JavaScript</strong>
+                    <div>
+                        JavaScript is a programming language
+                    </div>
+                </div>
+                <div className="post__btns">
+                    <button>
+                        Delete
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
