57dc93dab8983f61737f038719094d47862ffe31
VladislavOoll <apahiphip@mail.ru>
2023-04-04 21:58:26 +0300

    create: post item component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,16 @@ import React, {useState} from 'react';
 import Counter from './components/Counter';
 import ClassCounter from './components/ClassCounter';
 import './styles/App.css';
+import PostItem from './components/PostItem';
 
 function App() {
     const [value, setValue] = useState('Text in input');
 
     return (
         <div className="App">
-            <div className="post">
-                <div className="post__content">
-                    <strong>1. JavaScript</strong>
-                    <div>
-                        JavaScript is a programming language
-                    </div>
-                </div>
-                <div className="post__btns">
-                    <button>
-                        Delete
-                    </button>
-                </div>
-            </div>
+            <PostItem></PostItem>
+            <PostItem></PostItem>
+            <PostItem></PostItem>
         </div>
     );
 }
