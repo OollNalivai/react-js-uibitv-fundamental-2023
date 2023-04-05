74827f229739bdd2cb2647b5668c65e97c6e52ec
VladislavOoll <apahiphip@mail.ru>
2023-04-05 21:18:02 +0300

    create: props in component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, {useState} from 'react';
-import Counter from './components/Counter';
-import ClassCounter from './components/ClassCounter';
+import React from 'react';
+// import Counter from './components/Counter';
+// import ClassCounter from './components/ClassCounter';
 import './styles/App.css';
 import PostItem from './components/PostItem';
 
 function App() {
-    const [value, setValue] = useState('Text in input');
+    // const [value, setValue] = useState('Text in input');
 
     return (
         <div className="App">
-            <PostItem></PostItem>
-            <PostItem></PostItem>
-            <PostItem></PostItem>
+            <PostItem post={{
+                id: 1,
+                title: 'JavaScript',
+                body: 'JavaScript is a programming language'}}/>
         </div>
     );
 }
