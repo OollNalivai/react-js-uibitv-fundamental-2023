50d9e366acff5697f74ebf8537ae81652f27c330
VladislavOoll <apahiphip@mail.ru>
2023-04-06 23:26:22 +0300

    fetch: post list logic with map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import Counter from './components/Counter';
 // import ClassCounter from './components/ClassCounter';
 import './styles/App.css';
 import PostItem from './components/PostItem';
 
 function App() {
-    // const [value, setValue] = useState('Text in input');
+    const [posts, setPosts] = useState([
+        {
+            id: 1,
+            title: 'JavaScript',
+            body: 'JavaScript is a programming language'
+        },
+        {
+            id: 2,
+            title: 'JavaScript 2',
+            body: 'JavaScript is a programming language 2'
+        },
+        {
+            id: 3,
+            title: 'JavaScript 3',
+            body: 'JavaScript is a programming language 3'
+        }
+    ])
 
     return (
         <div className="App">
-            <PostItem post={{
-                id: 1,
-                title: 'JavaScript',
-                body: 'JavaScript is a programming language'}}/>
+            <h2 style={{textAlign: 'center'}}>Posts list</h2>
+            {posts.map((post) =>
+                <PostItem post={post} key={post.id}/>
+            )}
         </div>
     );
 }
