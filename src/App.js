3a0857bec0eacc2b342b747649f1b66d781f1821
VladislavOoll <apahiphip@mail.ru>
2023-04-08 23:22:08 +0300

    fetch: post list new component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 // import ClassCounter from './components/ClassCounter';
 import './styles/App.css';
 import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 
 function App() {
     const [posts, setPosts] = useState([
@@ -23,12 +24,28 @@ function App() {
         }
     ])
 
+    const [posts2, setPosts2] = useState([
+        {
+            id: 1,
+            title: 'Python',
+            body: 'Python is a programming language'
+        },
+        {
+            id: 2,
+            title: 'Python 2',
+            body: 'Python is a programming language 2'
+        },
+        {
+            id: 3,
+            title: 'Python 3',
+            body: 'Python is a programming language 3'
+        }
+    ])
+
     return (
         <div className="App">
-            <h2 style={{textAlign: 'center'}}>Posts list</h2>
-            {posts.map((post) =>
-                <PostItem post={post} key={post.id}/>
-            )}
+            <PostList posts={posts} title='Posts list'/>
+            <PostList posts={posts2} title='Posts list 2'/>
         </div>
     );
 }
