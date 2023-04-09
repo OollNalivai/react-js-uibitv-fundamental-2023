9964ba5af2087937d735ab9f3c975abeb0ebec35
VladislavOoll <apahiphip@mail.ru>
2023-04-09 20:35:19 +0300

    fetch: myInput and myButton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, {useState} from 'react';
-// import Counter from './components/Counter';
-// import ClassCounter from './components/ClassCounter';
 import './styles/App.css';
-import PostItem from './components/PostItem';
 import PostList from './components/PostList';
+import MyButton from './components/UI/Button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
     const [posts, setPosts] = useState([
@@ -24,28 +23,14 @@ function App() {
         }
     ])
 
-    const [posts2, setPosts2] = useState([
-        {
-            id: 1,
-            title: 'Python',
-            body: 'Python is a programming language'
-        },
-        {
-            id: 2,
-            title: 'Python 2',
-            body: 'Python is a programming language 2'
-        },
-        {
-            id: 3,
-            title: 'Python 3',
-            body: 'Python is a programming language 3'
-        }
-    ])
-
     return (
         <div className="App">
+            <form>
+                <MyInput type="text" placeholder={"Title post"}/>
+                <MyInput type="text" placeholder={"Text post"}/>
+                <MyButton>Create post</MyButton>
+            </form>
             <PostList posts={posts} title='Posts list'/>
-            <PostList posts={posts2} title='Posts list 2'/>
         </div>
     );
 }
