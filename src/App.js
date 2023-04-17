171c345daaf8f925269e0bb1983ee4cf710df22a
VladislavOoll <apahiphip@mail.ru>
2023-04-18 00:04:50 +0300

    refactoring: MyInput onChange logic and addNewPost

--- src/App.js
+++ src/App.js
@@ -23,20 +23,12 @@ function App() {
         }
     ]);
 
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
+    const [post, setPost] = useState({title: '', body: ''})
 
     const addNewPost = (e) => {
         e.preventDefault();
-        const newPost = {
-            id: Date.now(),
-            title,
-            body
-        };
-        setPosts([...posts, newPost]);
-        console.log(newPost);
-        setTitle('')
-        setBody('')
+        setPosts([...posts, {...post, id: Date.now()}]);
+        setPost({title: '', body: ''})
     };
 
     return (
@@ -45,14 +37,14 @@ function App() {
                 {/* Управляемый компонент*/}
 
                 <MyInput
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
+                    value={post.title}
+                    onChange={(e) => setPost({...post, title: e.target.value})}
                     type="text"
                     placeholder="Title post"/>
 
                 <MyInput
-                    value={body}
-                    onChange={(e) => setBody(e.target.value)}
+                    value={post.body}
+                    onChange={(e) => setPost({...post, body: e.target.value})}
                     type="text"
                     placeholder="Text post"/>
 
