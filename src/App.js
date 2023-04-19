c28d4ee8c07aacc836d51a339b041be39ff384ad
VladislavOoll <apahiphip@mail.ru>
2023-04-19 23:15:26 +0300

    fetch: create universal post form component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, {useState} from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
-import MyButton from './components/UI/Button/MyButton';
-import MyInput from './components/UI/input/MyInput';
+import PostForm from './components/PostForm';
 
 function App() {
     const [posts, setPosts] = useState([
@@ -23,35 +22,13 @@ function App() {
         }
     ]);
 
-    const [post, setPost] = useState({title: '', body: ''})
-
-    const addNewPost = (e) => {
-        e.preventDefault();
-        setPosts([...posts, {...post, id: Date.now()}]);
-        setPost({title: '', body: ''})
-    };
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
 
     return (
         <div className="App">
-            <form>
-                {/* Управляемый компонент*/}
-
-                <MyInput
-                    value={post.title}
-                    onChange={(e) => setPost({...post, title: e.target.value})}
-                    type="text"
-                    placeholder="Title post"/>
-
-                <MyInput
-                    value={post.body}
-                    onChange={(e) => setPost({...post, body: e.target.value})}
-                    type="text"
-                    placeholder="Text post"/>
-
-                <MyButton onClick={addNewPost}>Create post</MyButton>
-
-            </form>
-
+            <PostForm create={createPost}/>
             <PostList posts={posts} title="Posts list"/>
 
         </div>
